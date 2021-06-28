65ff3ceddf3148a31386c8c9e54773d420df0c85
LuisGtk <luisgonzalez189@gmail.com>
2021-06-28 09:19:19 -0400

    homepage

---
+++ calculator/src/App.js
@@ -0,0 +1,20 @@
+import Homepage from './components/Home';
+import { Route, Link } from 'react-router-dom'
+import './App.css';
+
+function app() {
+  return (
+    <div className="app">
+   <Homepage />
+   {/* <Route path="/" component={Homepage} /> */}
+
+
+
+
+
+ </div>
+
+  );
+}
+
+export default app;
---
+++ calculator/src/components/Calculator/Calculator.js
@@ -0,0 +1,7 @@
+export default function Calculator() {
+    return(
+        <div className="Calc-board">
+        
+        </div>
+    )
+}
