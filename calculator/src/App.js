e3424b6f6294da5b942a2ee30ac57ba35dcb83dd
LuisGtk <luisgonzalez189@gmail.com>
2021-06-30 16:05:18 -0400

    working on css

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -10,31 +10,29 @@ function App() {
        
 // HOME PAGE
   return (
-      <div> 
-      <h1>Welcome to Luis's Calculator App</h1>
-      <p>placeholder text</p>
-      <label for="Budget">Budget Tracker</label>
-      <input id="Budget" type="text" placeholder="Amount" autofocus required></input>
-      <button id ="Submit">submit</button>
-     <nav>
+ <div> 
+    <h1>Welcome to Luis's Calculator App</h1>
+    <p>Whether you want to know some weird facts or just want to calculate some stuff, this is the perfect place for you.</p>
+    <label for="Budget">Budget Tracker</label>
+    <input id="Budget" type="text" placeholder="Amount" autofocus required></input>
+    <button id ="Submit">submit</button>
+  <nav>
+    <main>
+{/* Making the open calculator button into a link */}
+{/* <Switch> */}
+ <Link to ="/Calculator">
+   <button id='OpenCalc'>Open Calculator</button>
+ </Link>  
 
-     <main>
-
-       {/* Making the open calculator button into a link */}
-
-       {/* <Switch> */}
-       <Link to ="/Calculator">
-      <button id='OpenCalc'>Open Calculator</button>
-      </Link>   
-      <Link to="Info" >
-     <button id="mathInfo">Learn More</button>
-      </Link>
-     <Route path="/Calculator" component={Calculator} />
-     <Route path="/Info" component={Info} />
-     {/* </Switch> */}
-     </main>
-     </nav>
-      </div>
+<Link to="Info" >
+  <button id="mathInfo">Learn More</button>
+</Link>
+<Route path="/Calculator" component={Calculator} />
+<Route path="/Info" component={Info} />
+{/* </Switch> */}
+    </main>
+  </nav>
+</div>
 
   );
 }
--- calculator/src/components/Info.js
+++ calculator/src/components/Info.js
@@ -1,33 +1,36 @@
 import { useEffect, useState } from "react"
 import "./Info.css"
+
+
 export default function Info () {
     // randomize order of random facts by looping over array up tp 30 times
-    const [num, setNum]= useState(Math.floor(Math.random() * 30))
-    const [randomFact, setRandomFact] = useState('')
+const [num, setNum]= useState(Math.floor(Math.random() * 30))
+const [randomFact, setRandomFact] = useState('')
 
-    const callApi = () => {
-        //grabbing data from url using num component from array  from second useState 
-        //above
-        fetch(`http://numbersapi.com/${num}`)
-        .then(res => res.text())
-        .then(data => setRandomFact(data))
+const callApi = () => {
+ //grabbing data from url using num component from array  from second useState 
+ //above
+     fetch(`http://numbersapi.com/${num}`)
+     .then(res => res.text())
+     .then(data => setRandomFact(data))
     }
 
-    useEffect(() => {
-callApi()
-    }, [num])
+ useEffect(() => {
+    callApi()
+     }, [num])
 
-    const handleClick =() => {
-        setNum(num => num+1)
-
+//incrementing the number of facts by one after reach click
+const handleClick =() => {
+    setNum(num => num+1)
     }
+console.log(num)
 
-    console.log(num)
-    return(
-        <>
-        <h1>Get your random number facts here</h1>
-        {randomFact}
-        <button onClick={handleClick}>get fact</button>
-        </>
+return(
+ <>
+   <h1>Get your random number facts here</h1>
+   <button className="facts"onClick={handleClick}>Click for another fun fact</button>
+   {/* randomFact variable being called */}
+   {randomFact}
+</>
     )
 }
