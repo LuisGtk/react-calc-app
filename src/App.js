f8bb735ca05a43ff867d9cc50d7867876aac876d
LuisGtk <luisgonzalez189@gmail.com>
2021-10-12 11:29:19 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import { Route, Link, Switch } from 'react-router-dom'
 import Calculator from './components/Calculator/Calculator';
-import React,{ useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Info from './components/Info';
 import './App.css';
 
 function App() {
-       
-// HOME PAGE
+
+  // HOME PAGE
   return (
- <div> 
-  <nav>
-   <Link to = '/'>
-      <h1>Welcome to Luis's Calculator App</h1>
-   </Link>
-      <p>Whether you want to know some weird facts or just want to calculate some stuff, this is the perfect place for you.</p>
-  <main>
-{/* Making the open calculator button into a link */}
- <Link to ="/Calculator">
-   <button id='OpenCalc'>Open Calculator</button>
- </Link>  
+    <div>
+      <nav>
+        <Link to='/'>
+          <h1>Welcome to Luis's Calculator App</h1>
+        </Link>
+        <p>Whether you want to know some weird facts or just want to calculate some stuff, this is the perfect place for you.</p>
+        <main>
+          {/* Making the open calculator button into a link */}
+          <Link to="/Calculator">
+            <button id='OpenCalc'>Open Calculator</button>
+          </Link>
 
-<Link to="Info" >
-  <button id="mathInfo">Click for some facts</button>
-</Link>
+          <Link to="Info" >
+            <button id="mathInfo">Click for some facts</button>
+          </Link>
 
-  </main>
-  </nav>
-  {/* component will only render the first route that matches/includes the path */}
-<Switch>
-<Route path="/Calculator" component={Calculator} />
-<Route path="/Info" component={Info} />
-</Switch>
-</div>
+        </main>
+      </nav>
+      {/* component will only render the first route that matches/includes the path */}
+      <Switch>
+        <Route path="/Calculator" component={Calculator} />
+        <Route path="/Info" component={Info} />
+      </Switch>
+    </div>
   );
 }
 export default App;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,33 +1,33 @@
 import { useEffect, useState } from "react"
 import "./Info.css"
 
-export default function Info () {
- 
-const [num, setNum]= useState(Math.floor(Math.random() * 30))
-const [randomFact, setRandomFact] = useState('')
+export default function Info() {
 
-const callApi = () => {
- //grabbing data from url using num component from array  from second useState 
- //above
-     fetch(`http://numbersapi.com/${num}`)
-     .then(res => res.text())
-     .then(data => setRandomFact(data))
-    }
-  useEffect(() => {
-     callApi()
-     }, [num])
+  const [num, setNum] = useState(Math.floor(Math.random() * 30))
+  const [randomFact, setRandomFact] = useState('')
 
-//incrementing the number of facts by one after each clickP
-const handleClick =() => {
-  setNum(num => num+1)
+  const callApi = () => {
+    //grabbing data from url using num component from array  from second useState 
+    //above
+    fetch(`http://numbersapi.com/${num}`)
+      .then(res => res.text())
+      .then(data => setRandomFact(data))
   }
-   //console.log(num)
-return(
- <>
-   <h1>Get your random number generated facts here</h1>
-   <button className="facts"onClick={handleClick}>Click for another fun fact</button>
-   {/* randomFact variable being called */}
-   <p> {randomFact}</p>
-</>
-    )
+  useEffect(() => {
+    callApi()
+  }, [num])
+
+  //incrementing the number of facts by one after each click
+  const handleClick = () => {
+    setNum(num => num + 1)
+  }
+  //console.log(num)
+  return (
+    <>
+      <h1>Get your random number generated facts here</h1>
+      <button className="facts" onClick={handleClick}>Click for another fun fact</button>
+      {/* randomFact variable being called */}
+      <p> {randomFact}</p>
+    </>
+  )
 }
